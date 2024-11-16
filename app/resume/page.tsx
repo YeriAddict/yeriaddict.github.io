731538ce6f4af3179d72d4dbea111dd419f47c96
YeriAddict <denis.leang@gmail.com>
2024-11-15 23:18:37 -0500

    Updated resume page

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,23 +1,39 @@
+import { Button } from '@nextui-org/button';
+import { Card, CardBody } from '@nextui-org/card';
+import { Link } from '@nextui-org/link';
+import { Spacer } from '@nextui-org/spacer';
+import Image from 'next/image';
+
 export default function ResumePage() {
   return (
-    <div style={{ width: "100%", height: "160vh" }}>
-      <embed
-        style={{
-          width: "100%",
-          height: "80%",
-        }}
-        type="application/pdf"
-        src="/Resume.pdf"
-      />
-      <div style={{ textAlign: "center", marginTop: "10px" }}>
-        <a
-          href="/Resume.pdf"
-          download="Resume.pdf"
-          style={{ fontSize: "18px", color: "#006FEE" }}
-        >
-          Download Resume
-        </a>
-      </div>
+    <div className="flex flex-col items-center gap-5">
+      <Card
+        isFooterBlurred
+        radius="none"
+        className="border-none"
+      >
+        <Image
+          alt="Woman listing to music"
+          className="object-cover"
+          src="resume/resume.png"
+          height={700}
+          width={700}
+        />
+
+      </Card>
+
+      <Button
+        as={Link}
+        href="/resume/resume.pdf"
+        download="Resume.pdf"
+        color="primary"
+        variant="solid"
+        style={{ width: '200px', textAlign: 'center' }}
+      >
+        Download
+      </Button>
+
+      <Spacer y={1} />
     </div>
   );
 }
