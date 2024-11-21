9d4623c53896d54a02e19df35842df56f2330a56
YeriAddict <denis.leang@gmail.com>
2024-11-21 01:32:23 -0500

    Added music tab

--- components/cards/hobbies/music-card-content.tsx
+++ components/cards/hobbies/music-card-content.tsx
@@ -1,5 +1,44 @@
+import { Spacer } from "@nextui-org/spacer";
+
 export const MusicCardContent = {
   title: "🎼 Music",
-  cardBody: <>TODO</>,
-  modalBody: <>TODO</>,
+  cardBody: (
+    <>
+      <div className="flex flex-col">
+        <p>🎵 I am addicted to music!</p>
+        <Spacer y={6} />
+        <p>
+          🎹 I probably spend 80% of my day listening to music. If my ears never
+          got tired, I would even listen while sleeping.
+        </p>
+        <Spacer y={6} />
+        <p>
+          🎻 I enjoy all genres of music, whether it is French chansons,
+          mainstream pop, or even Russian rap. Always happy to discover!
+        </p>
+        <Spacer y={6} />
+        <p>🎶 K-pop is my favorite, though!</p>
+      </div>
+    </>
+  ),
+  modalBody: (
+    <>
+      <p>My current favorite songs:</p>
+      <div className="flex flex-row gap-4 justify-between">
+        <iframe
+          className="w-1/2"
+          height={400}
+          src="https://www.youtube.com/embed/X7hMrLuWgBk"
+          title="colouring"
+        />
+        <iframe
+          className="w-1/2"
+          height={400}
+          src="https://www.youtube.com/embed/pCsyyQoyEDU"
+          title="last_drop"
+        />
+      </div>
+      <p>Honorable mention: Red Velvet 🙂</p>
+    </>
+  ),
 };
