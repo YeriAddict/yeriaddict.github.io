9987d097fc98fd0a903837a1d3a4af5ed0d44e39
YeriAddict <denis.leang@gmail.com>
2024-11-21 02:21:05 -0500

    Added fun facts

--- components/tabs/tab/home/facts-tab.tsx
+++ components/tabs/tab/home/facts-tab.tsx
@@ -4,7 +4,33 @@ export const FactsTab = () => {
   return (
     <>
       <Card>
-        <CardBody>Lorem ipsum doasdsadaasdadconsequat.</CardBody>
+        <CardBody>
+          <h4 className="text-lg font-bold mb-4">The ones I am proud of:</h4>
+          <ul className="list-disc list-inside space-y-2">
+            <li>I was born in the Year of the Rabbit 🐰</li>
+            <li>Bubble tea always fixes everything for me 🧋</li>
+            <li>I always go for a walk to refresh my mind 🚶‍♂️</li>
+            <li>I love Vietnamese food 🍜</li>
+          </ul>
+
+          <h4 className="text-lg font-bold mt-6 mb-4">
+            The ones I am not proud of:
+          </h4>
+          <ul className="list-disc list-inside space-y-2">
+            <li>
+              When I was 4, I sneaked away to play DOOM (a violent shooting
+              game) 🥷
+            </li>
+            <li>
+              I accidentally broke someone&apos;s finger while playing handball
+              ⛹️‍♂️
+            </li>
+            <li>
+              I had a pretty bad reflex syncope (loss of consciousness), which
+              made my loved ones worry about me 🤕
+            </li>
+          </ul>
+        </CardBody>
       </Card>
     </>
   );
